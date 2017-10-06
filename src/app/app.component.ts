56c0a77a2ca5f91c7ec5412542aaa404dbc4397c
Joan Albert Segura <jalbertsr@protonmail.ch>
2017-10-06 20:33:42 +0200

    created repo section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { StarPage } from '../pages/stars/stars';
 
 @Component({
   templateUrl: 'app.html'
@@ -22,7 +23,8 @@ export class MyApp {
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: 'Repos', component: ListPage },
+      { title: 'Stars', component: StarPage }
     ];
 
   }
